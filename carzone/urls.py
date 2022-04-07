9dc168a76bd695f25d382976412d416b41082cfa
Islom <islom.abdullaev.02@mail.ru>
2022-04-08 00:07:15 +0500

    ready to deploy

--- carzone/urls.py
+++ carzone/urls.py
@@ -30,3 +30,4 @@ urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
